ab6b67e53b4f99d1a3c0a836865fd1975a1c5ae8
Ian Dunn <iondune@gmail.com>
2016-04-06 15:21:44 -0700

    Experimenting with ear clipping

--- LineRendering/DemoScene.cpp
+++ LineRendering/DemoScene.cpp
@@ -41,6 +41,99 @@ ion::Scene::CSimpleMesh * CreateLineMesh(vector<vec3f> const & Points, vec3f con
 	return Mesh;
 }
 
+float Determinant(vec2f const & u, vec3f const & v)
+{
+	return u.X*v.Y - u.Y*v.X;
+}
+
+bool IsPointInTriangle(vec2f const & a, vec2f const & b, vec2f const & c, vec2f const & point)
+{
+	if (Determinant(b - a, point - a) > 0 && Determinant(c - b, point - b) > 0 && Determinant(a - c, point - c) > 0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+vector<vec2f> TriangulateEarClipping(vector<vec2f> const & OriginalPoints)
+{
+	vector<vec2f> Return;
+	vector<vec2f> Points = OriginalPoints;
+
+	if (Points.size() >= 3)
+	{
+		bool TriangleFound = true;
+
+		while (Points.size() != 0)
+		{
+			if (! TriangleFound)
+				return Return;
+
+			TriangleFound = false;
+
+			for (int i(0); i < Points.size() - 2; i++)
+			{
+				if (! TriangleFound)
+				{
+					bool result = false;
+					if (Determinant(Points[i + 1] - Points[i], Points[i + 2] - Points[i + 1]) < 0)
+					{
+						result = true;
+						for (int j(0); j < OriginalPoints.size(); j++)
+						{
+							if (IsPointInTriangle(Points[i + 2], Points[i + 1], Points[i], OriginalPoints[j]))
+							{
+								result = false;
+							}
+						}
+					}
+
+					if (result)
+					{
+						TriangleFound = true;
+
+						Return.push_back(Points[i]);
+						Return.push_back(Points[i + 1]);
+						Return.push_back(Points[i + 2]);
+
+						Points.erase(Points.begin() + i + 1);
+					}
+				}
+			}
+		}
+	}
+
+	return Return;
+}
+
+
+ion::Scene::CSimpleMesh * CreatePolygonMesh(vector<vec2f> const & Points)
+{
+	ion::Scene::CSimpleMesh * Mesh = new ion::Scene::CSimpleMesh();
+
+	vector<vec2f> Triangles = TriangulateEarClipping(Points);
+
+	for (size_t i = 0; i + 2 < Triangles.size(); i += 3)
+	{
+		uint const Start = (uint) Mesh->Vertices.size();
+		vec3f const Normal = vec3f(0, 1, 0);
+		Mesh->Vertices.push_back(CSimpleMesh::SVertex(vec3f(Triangles[i].X, 0, Triangles[i].Y), Normal));
+		Mesh->Vertices.push_back(CSimpleMesh::SVertex(vec3f(Triangles[i+1].X, 0, Triangles[i+1].Y), Normal));
+		Mesh->Vertices.push_back(CSimpleMesh::SVertex(vec3f(Triangles[i+2].X, 0, Triangles[i+2].Y), Normal));
+
+		CSimpleMesh::STriangle Triangle;
+		Triangle.Indices[0] = Start + 0;
+		Triangle.Indices[1] = Start + 1;
+		Triangle.Indices[2] = Start + 2;
+		Mesh->Triangles.push_back(Triangle);
+	}
+
+	return Mesh;
+}
+
 int main()
 {
 	////////////////////
@@ -121,14 +214,20 @@ int main()
 	RenderPass->AddSceneObject(SceneObject4);
 
 	srand(1234);
-	vec3f const Center = vec3f(0, -50, 0);
-	vector<vec3f> Points;
-	Points.push_back(vec3f(0, -1, 0));
+	vector<vec2f> Points;
+	Points.push_back(vec2f(0, 0));
+	Points.push_back(vec2f(0, 2));
+	Points.push_back(vec2f(1, 3));
+	Points.push_back(vec2f(2, 2));
+	Points.push_back(vec2f(3, 1));
+	Points.push_back(vec2f(2, 0));
 
 	for (uint i = 0; i < 12; ++ i)
 	{
-		Points.push_back(Points.back() + vec3f(nrand()*(frand() + 1), 0, nrand()*(frand() + 1)));
+		//Points.push_back(Points.back() + vec2f(nrand()*(frand() + 1), nrand()*(frand() + 1)));
 	}
+
+	SceneObject4->SetMesh(CreatePolygonMesh(Points));
 
 	///////////////
 	// Main Loop //
@@ -141,7 +240,6 @@ int main()
 		time += 0.01f;
 		RenderTarget->ClearColorAndDepth();
 
-		SceneObject4->SetMesh(CreateLineMesh(Points, Center, 0.1f, 0.05f*(1 - cos(time))));
 
 		SceneManager->DrawAll();
 		Window->SwapBuffers();
